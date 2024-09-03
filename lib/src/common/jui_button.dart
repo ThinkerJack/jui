ef7266d173a141433bd90adbcc1468d6d67ac135
wuchao <54969139+ThinkerJack@users.noreply.github.com>
2024-09-03 20:45:23 +0800

    feature#文件重构

--- lib/src/common/jui_button.dart
+++ lib/src/common/jui_button.dart
@@ -4,7 +4,51 @@ import '../utils/jui_theme.dart';
 enum JuiButtonSizeType { large, middle, small, ultraSmall }
 enum JuiButtonColorType { blue, gray, white, blueBorder }
 
+class JuiButtonSizeConfig {
+  final double height;
+  final double fontSize;
+  final double padding;
+
+  const JuiButtonSizeConfig({required this.height, required this.fontSize, required this.padding});
+}
+
+class JuiButtonColorConfig {
+  final Color Function(bool disable) getColor;
+  final Color Function(bool disable) getFontColor;
+  final BoxBorder? border;
+
+  const JuiButtonColorConfig({required this.getColor, required this.getFontColor, this.border});
+}
+
 class JuiButton extends StatelessWidget {
+  static final Map<JuiButtonSizeType, JuiButtonSizeConfig> _sizeConfig = {
+    JuiButtonSizeType.large: const JuiButtonSizeConfig(height: 48, fontSize: 16, padding: 32),
+    JuiButtonSizeType.middle: const JuiButtonSizeConfig(height: 40, fontSize: 14, padding: 24),
+    JuiButtonSizeType.small: const JuiButtonSizeConfig(height: 32, fontSize: 14, padding: 16),
+    JuiButtonSizeType.ultraSmall: const JuiButtonSizeConfig(height: 24, fontSize: 12, padding: 12),
+  };
+
+  static Map<JuiButtonColorType, JuiButtonColorConfig> get _colorConfig => {
+    JuiButtonColorType.white: JuiButtonColorConfig(
+      getColor: (_) => JuiTheme.colors.surface,
+      getFontColor: (disable) => disable ? JuiTheme.colors.disabledLight : JuiTheme.colors.text,
+      border: Border.all(color: JuiTheme.colors.divider, width: 1),
+    ),
+    JuiButtonColorType.gray: JuiButtonColorConfig(
+      getColor: (_) => JuiTheme.colors.background,
+      getFontColor: (disable) => disable ? JuiTheme.colors.disabledLight : JuiTheme.colors.text,
+    ),
+    JuiButtonColorType.blue: JuiButtonColorConfig(
+      getColor: (disable) => disable ? JuiTheme.colors.lightBlue : JuiTheme.colors.primary,
+      getFontColor: (_) => JuiTheme.colors.surface,
+    ),
+    JuiButtonColorType.blueBorder: JuiButtonColorConfig(
+      getColor: (_) => JuiTheme.colors.surface,
+      getFontColor: (_) => JuiTheme.colors.primary,
+      border: Border.all(color: JuiTheme.colors.primary, width: 1),
+    ),
+  };
+
   const JuiButton({
     Key? key,
     required this.colorType,
@@ -34,6 +78,10 @@ class JuiButton extends StatelessWidget {
   final double fontHeight;
   final Color? backGroundColor;
 
+  static void updateSizeConfig(JuiButtonSizeType type, JuiButtonSizeConfig config) {
+    _sizeConfig[type] = config;
+  }
+
   @override
   Widget build(BuildContext context) {
     if (!visibility) return const SizedBox.shrink();
@@ -45,8 +93,8 @@ class JuiButton extends StatelessWidget {
   }
 
   Widget _buildButtonContainer() {
-    final sizeConfig = JuiTheme.buttonConfig.sizeConfig[sizeType]!;
-    final colorConfig = JuiTheme.buttonConfig.colorConfig[colorType]!;
+    final sizeConfig = _sizeConfig[sizeType]!;
+    final colorConfig = _colorConfig[colorType]!;
 
     final buttonContent = Container(
       height: height ?? sizeConfig.height,
